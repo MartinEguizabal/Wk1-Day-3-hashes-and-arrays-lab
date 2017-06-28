0bf811db8460659f64488efa77e0d36d834fa3f1
MartyCodeClan <zabbalab@hotmail.com>
2017-06-28 15:28:59 +0100

    question two commit

--- my_functions.rb
+++ my_functions.rb
@@ -2,3 +2,11 @@ def add_array_lengths(array1, array2)
  return (array1 + array2).count
 end
 
+
+def sum_array(numbers)
+  sum_value = 0
+  for number in numbers
+    sum_value += number
+  end
+  return sum_value
+end
